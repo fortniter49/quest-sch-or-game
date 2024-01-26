6c846cb5500251c1598c5f12ff945c523384519a
Kitaev Nikita <150126251+fortniter49@users.noreply.github.com>
2024-01-26 03:16:38 +0300

    Update quest.py

--- quest.py
+++ quest.py
@@ -1,12 +1,14 @@
 a = input()
-#y1 - калькулятор
-#y2 - подушка
-#y3 - набор химика
-#y4 - переводчик
-#y5 - кофе
+print(' 1 - калькулятор')
+print(' 2 - подушка')
+print(' 3 - набор химика')
+print(' 4 - переводчик')
+print(' 5 - кофе')
+y1 = input()
+y2 = input()
 if a == '1': 
     a1 = input()
-    if a1 == '1': #PC
+    if a1 == '1':  #PC
         a2 = input()
         if a2 == '1':  #GAME
             a3 = input()
@@ -27,10 +29,54 @@ if a == '1':
                             x111 += 1
                 if x222 == 3:
                     if x111 == 2:
-                        print('lose')
-                elif x222 == 0 and x111 == 0 or x111 == 1:
+                        if y1 != '2' and y2 != '2':
+                            print('end')  #END
+                        else:
+                            print('end')  #END
+                if x222 == 0 and x111 == 0 or x111 == 1:
                     print ('вам доступен супер учебник по геометрии')
                     geom = 1
-                elif x222 == 0 and x111 == 2:
+                if x222 == 0 and x111 == 2:
                     print('вам доступен супер учебник по русскому')
                     russ = 1
+                a4 = input()
+                if a4 == '1':
+                    print('end')  #END
+                elif a4 == '2':
+                    o0 = 1
+            elif a3 == '2':  #PCGAME
+                a4 = input()
+                if a4 == '1' or a4 == '2':  #CSFORT
+                    a5 = input()
+                    if a5 == '1':
+                        print('end')  #END
+                    elif a5 == '2':
+                        o0 = 1
+                elif a4 == '3':
+                    a5 = input()
+                    if a5 == '1':
+                        print('end')  #END
+                    elif a5 == '2':
+                        o0 == 1
+        elif a2 == '2':  #FILM
+            a3 = input()
+            if a3 == '1':  #FILM
+                a4 = input()
+                if a4 == '1':
+                    print('доступен код')
+                    kod = 1
+                elif a4 == '2':
+                    print('вы потеряли ...')
+                    y2 = 0
+                elif a4 == '3':
+                    print('сон')  #END
+                if a4 == '1' or a4 == '2':
+                    a5 = input()
+                    if a5 == '1':
+                        print(end)  #END
+                    else:
+                        o0 == 1
+            elif a3 == '2':  #series
+                ...
+    elif a2 == '2':  #SCH
+        print('s')
