5e9091da066a39e464feeb138a68ba2f9fc0f785
Kitaev Nikita <150126251+fortniter49@users.noreply.github.com>
2024-02-06 00:23:50 +0300

    Update quest.py

--- quest.py
+++ quest.py
@@ -2,6 +2,10 @@ print(' Приветствую вас в моем квесте,')
 print(' Ваша задача не только сдать зачеты,')
 print(' Но и развлечся')
 print(' Нажмите 1 для старта')
+end = 0
+asd = 0
+win = 0
+kod = 0
 qweewq = 0
 chem = 0
 phis = 0
@@ -10,8 +14,9 @@ health = 3
 final = 0
 geom = 0
 russ = 0
+eng = 0
 finish = set('')
-o0 = 1
+o0 = 0
 a = input()
 if a == '1':
     print(' Выберите 2 предмета на игру')
@@ -28,6 +33,7 @@ if a == '1':
         health += 1
     if y1 == '4' or y2 == '4':
         finish.add('Английский ')
+        eng = 1
     if y1 == '2' or y2 == '2':
         pod = 1
     if y1 == '3' or y2 == '3':
@@ -67,12 +73,14 @@ if a == '1':
                         if y1 != '2' and y2 != '2':
                             print(' Вы вышли из сооружения и заснули, но')
                             print(' После вы проснулись в лучшем мире')
+                            end = 1
                             input()
                         else:
                             print(' Вы вышли из сооружения и заснули, но')
                             print(' Вы упали на суперподушку')
                             print(' Которая дала вам знаний,')
                             print(' С помощью нее вы все сдали на 5')
+                            end = 1
                             input()
                         qweewq = 1
                 if x222 == 0 and x111 == 0 or x111 == 1:
@@ -101,6 +109,7 @@ if a == '1':
                     if a4 == '1' and y1 != 4 and y2 != 4:
                         print(' Вы получили навык английского языка')
                         finish.add('Английский ')
+                        eng = 1
                     print(' 1 - пойти спать')
                     print(' 2 - пойти играть дальше')
                     print(' 3 - пойти учить уроки')
@@ -115,6 +124,7 @@ if a == '1':
                 elif a4 == '3':
                     print('!!!!! Вы стали жестоким и не пошли')
                     print('!!!!! Сдавать зачеты')
+                    end = 1
                     input()
         elif a2 == '2':
             print(' ФИЛЬМ / СЕРИАЛ ')
@@ -128,12 +138,15 @@ if a == '1':
                 if a4 == '1':
                     print(' Вам доступен новый код')
                     finish.add('Информаттику ')
+                    kod = 1
                 elif a4 == '2':
                     print(' Тебе выдан штраф')
                     print(' За этот ответ')
                     finish = set('')
+                    health -= 1
                 elif a4 == '3':
                     print(' Вы заснули и проспали')
+                    end = 1
                     input()
                 if a4 == '1' or a4 == '2':
                     print(' Пойти спать / делать уроки')
@@ -152,28 +165,33 @@ if a == '1':
                     if chem == 1:
                         print(' Вы улучшили свои навыки в химии')
                         chem += 1
+                        o0 = 1
                     else:
                         print(' Вы улучшили свои навыки в химии')
                         chem = 1
+                        o0 = 1
                 elif a4 == '2':
                     print(' Вы улучшили свои навыки в физике')
                     phis = 1
+                    o0 = 1
                 elif a4 == '3':
                     print(' Ну это просто...')
+                    end = 1
                     input()
     if a1 == '2' or o0 == 1:
         while health != 0:
-            a2 = input()
             print(' 1 - Математика')
             print(' 2 - Языки')
             print(' 3 - Остальное')
+            a2 = input()
             if a2 == '1':
-                a3 = input()
                 print(' 1 - Геометрия')
                 print(' 2 - Алгебра')
                 print(' 3 - Информатика')
+                a3 = input()
                 if a3 == '1':
                     if geom == '1':
+                        print('Вы выучили геометрию')
                         finish.add('Геометрию ')
                     else:
                         print(' Катит это ...')
@@ -184,13 +202,17 @@ if a == '1':
                         if a4 == '1':
                             finish.add('Геометрию ')
                             print(' Вы выучили геометрию')
+                            health -= 1
                         elif a4 == '2':
                             print('Гений')
+                            end = 1
+                            health = 0
                             input()
                         elif a4 == '3':
                             finish.add('Геометрию')
                             finish.add('Русский ')
                             print('Вы выучили геометрию и русский')
+                            asd = 1
                             health -= 1
                 elif a3 == '2':
                     if y1 == '1' or y2 == '1':
@@ -198,3 +220,95 @@ if a == '1':
                         health -= 1
                     else:
                         print(' √1936 = ...')
+                        print(' 1 - 46')
+                        print(' 2 - 44')
+                        print(' 3 - 34')
+                        a4 = input()
+                        if a4 == '1':
+                            print('Вы не выучили алгебру')
+                            health -= 1
+                        elif a4 == '2':
+                            print('Вы выучили алгебру')
+                            finish.add('Алгебру ')
+                            health -= 1
+                        elif a4 == '3':
+                            print('Вы не выучили алгебру')
+                            health -= 1
+                elif a3 == '3':
+                    if kod == '1':
+                        print('Вы выучили информатику')
+                    else:
+                        print('3 самых известных языка программирования')
+                        print('1 - JavaScript, Python, Java')
+                        print('2 - C++, Java, Python')
+                        print('3 - JavaScript, C++, Python')
+                        a4 = input()
+                        if a4 == '1':
+                            print('Вы выучили информатику')
+                            finish.add('Информатику ')
+                        elif a4 == '2':
+                            print('Вы не выучили информатику')
+                        elif a4 == '3':
+                            print('Вы не выучили информатику')
+                        health -= 1
+            elif a2 == '2':
+                print('1 - Русский')
+                print('2 - Ангийский')
+                a3 = input()
+                if a3 == '1':
+                    if russ == 1:
+                        print('Вы выучили Русский')
+                        finish.add('Русский ')
+                    elif asd == 1:
+                        print('Вы уже выучили Русский')
+                        health -= 1
+                    else:
+                        print('Синтаксис это ...')
+                        print('1 - совокупность, чет такое')
+                        print('2 - сынтаксист')
+                        print('3 - не это')
+                        a4 = input()
+                        if a4 == '1':
+                            print('Вы выучили Русский')
+                            finish.add('Русский ')
+                            health -= 1
+                        elif a4 == '2':
+                            print('Вы выучили жизнь')
+                            win = 1
+                            health = 0
+                        elif a4 == '3':
+                            print('Эхххх')
+                            print('Ну ты проиграл')
+                            end = 1
+                            health = 0
+                            input()
+                elif a3 == '2':
+                    if eng == 1:
+                        print('Вам не нужно учить английский')
+                    else:
+                        print('Халоу, хау ар ю?')
+                        print('1 - Айм окэй')
+                        print('2 - кОмсИ коМСа')
+                        print('3 - че')
+                        a4 = input()
+                        if a4 == '1':
+                            print('Молодец емае')
+                            finish.add('Английский ')
+                            health -= 1
+                        elif a4 == '2':
+                            print('ААААААА Француз! Француз!')
+                            health -= 1
+                        elif a4 == '3':
+                            print('а ниче')
+                            health -= 1
+            elif a2 == '3':
+                print('1 - Химия')
+                print('2 - Физика')
+                print('3 - География')
+                a3 = input()
+                if a3 == '1':
+                    if chem == 2:
+                        print('Вы стали Хайзенбергом')
+                        end = 1
+                        health = 0
+                        input()
